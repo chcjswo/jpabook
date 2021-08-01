91aff57aac1311118c1405379c026a5ee5b0640a
tony <chcjswo@gmail.com>
2021-08-02 00:07:55 +0900

    item type enum 추가

---
+++ src/main/java/com/mocadev/jpabook/jpashop/domain/item/ItemType.java
@@ -0,0 +1,19 @@
+package com.mocadev.jpabook.jpashop.domain.item;
+
+/**
+ * @author chcjswo
+ * @version 1.0.0
+ * @blog https://mocadev.tistory.com
+ * @github https://github.com/chcjswo
+ * @since 2021-08-02
+ **/
+public enum ItemType {
+
+	BOOK("도서"), FOOD("음식"), ETC("기타");
+
+	private final String description;
+
+	ItemType(String description) {
+		this.description = description;
+	}
+}
