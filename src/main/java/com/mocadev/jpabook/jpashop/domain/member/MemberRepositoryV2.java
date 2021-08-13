f78eff1948bff43c480f72b1a1d71165681f8d54
tony <chcjswo@gmail.com>
2021-08-13 23:37:20 +0900

    회원가입 폼 추가 bean name conflict 해결

--- src/main/java/com/mocadev/jpabook/jpashop/domain/member/MemberRepositoryV2.java
+++ src/main/java/com/mocadev/jpabook/jpashop/domain/member/MemberRepositoryV2.java
@@ -1,13 +1,16 @@
 package com.mocadev.jpabook.jpashop.domain.member;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
-import java.util.*;
-
 @Slf4j
 @Repository
-public class MemberRepository {
+public class MemberRepositoryV2 {
 
 	private static Map<Long, Member> store = new HashMap<>();
 	private static long sequence = 0;
--- src/main/java/com/mocadev/jpabook/jpashop/web/member/MemberControllerV2.java
+++ src/main/java/com/mocadev/jpabook/jpashop/web/member/MemberControllerV2.java
@@ -1,23 +1,22 @@
 package com.mocadev.jpabook.jpashop.web.member;
 
-import com.mocadev.jpabook.jpashop.domain.member.MemberRepository;
+import com.mocadev.jpabook.jpashop.domain.member.Member;
+import com.mocadev.jpabook.jpashop.domain.member.MemberRepositoryV2;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.mocadev.jpabook.jpashop.domain.member.Member;
-
-import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/members")
-public class MemberController {
+public class MemberControllerV2 {
 
-	private final MemberRepository memberRepository;
+	private final MemberRepositoryV2 memberRepository;
 
 	@GetMapping("/add")
 	public String addForm(@ModelAttribute("member") Member member) {
