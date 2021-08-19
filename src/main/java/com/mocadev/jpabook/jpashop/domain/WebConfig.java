03f9e9c7bf71d7be86465e06a742a8148cfaccf3
tony <chcjswo@gmail.com>
2021-08-19 23:40:17 +0900

    Log Filter 적용

---
+++ src/main/java/com/mocadev/jpabook/jpashop/domain/WebConfig.java
@@ -0,0 +1,28 @@
+package com.mocadev.jpabook.jpashop.domain;
+
+import com.mocadev.jpabook.jpashop.web.filter.LogFilter;
+import javax.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author chcjswo
+ * @version 1.0.0
+ * @blog https://mocadev.tistory.com
+ * @github https://github.com/chcjswo
+ * @since 2021-08-19
+ **/
+@Configuration
+public class WebConfig {
+
+	@Bean
+	public FilterRegistrationBean logFilter() {
+		FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+		filterFilterRegistrationBean.setFilter(new LogFilter());
+		filterFilterRegistrationBean.setOrder(1);
+		filterFilterRegistrationBean.addUrlPatterns("/*");
+		return filterFilterRegistrationBean;
+	}
+
+}
