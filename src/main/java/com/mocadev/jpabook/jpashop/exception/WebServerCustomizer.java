1b0e3d8eb5ce244007bb76cda330dc0583e934e0
tony <chcjswo@gmail.com>
2021-08-25 22:17:38 +0900

    커스터 마이징 오류 페이지 사용 불가 처리

--- src/main/java/com/mocadev/jpabook/jpashop/exception/WebServerCustomizer.java
+++ src/main/java/com/mocadev/jpabook/jpashop/exception/WebServerCustomizer.java
@@ -4,7 +4,6 @@ import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
 
 /**
  * @author chcjswo
@@ -13,7 +12,7 @@ import org.springframework.stereotype.Component;
  * @github https://github.com/chcjswo
  * @since 2021-08-22
  **/
-@Component
+//@Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
 	@Override
