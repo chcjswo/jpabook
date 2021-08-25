9c62c7f63cc8fc09a6cbf09e2c35392e967f25f0
tony <chcjswo@gmail.com>
2021-08-25 21:37:18 +0900

    스프링 부트 기본 오류 처리

--- src/main/java/com/mocadev/jpabook/jpashop/exception/servlet/ErrorPageController.java
+++ src/main/java/com/mocadev/jpabook/jpashop/exception/servlet/ErrorPageController.java
@@ -1,8 +1,14 @@
 package com.mocadev.jpabook.jpashop.exception.servlet;
 
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -25,6 +31,18 @@ public class ErrorPageController {
 	@RequestMapping("/error-page/500")
 	public String errorPage500(HttpServletRequest request, HttpServletResponse response) {
 		return "error-page/500";
+	}
+
+	@RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Map<String, Object>> errorPage500Json(HttpServletRequest request, HttpServletResponse response) {
+		log.info("API error 500");
+		Map<Object, Object> map = new HashMap<>();
+		Exception error = (Exception) request.getAttribute("javax.servlet.error.exception");
+		map.put("status", request.getAttribute("javax.servlet.error.status_code"));
+		map.put("message", error.getMessage());
+		Integer statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+		return new ResponseEntity(map, HttpStatus.valueOf(statusCode));
 	}
 
 }
